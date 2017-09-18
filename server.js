9ca8f13f0de59c2698c26e24177351ce5573ea25
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-09-18 18:03:44 -0400

    routes set up for two main pages now need to set up routes for api and link to github

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require ("path");
+// var path = require ("path");
 
 var port = process.env.PORT || 3000;
 
 var app = express(); 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
  
 //create routes for files
+var func = require("./app/routing/htmlroutes");
+func(app);
+
+
+
+app.listen(port, function() {
+  console.log("App listening on PORT: " + port);
+});
