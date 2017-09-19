9a7bfee3017874b3e6c53f0da2366113bc8ae6bc
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-09-18 23:08:21 -0400

    code is not working the way i wanted it too grr

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,12 +7,12 @@ var friendsArr = [
     {
     name: "Sasha",
     bio: "You'd much rather spend your friday nights in with Netflix and Chinese food. Sasha does too! Shoot her a message.",
-    personality: "extrovert",
+    personality: "introvert",
   },
   {
     name: "Ted",
     bio: "Sounds like you're outgoing but love your alone time as well. Ted sounds like a perfect match to your personality. Send him a message!",
-    personality: "extrovert",
+    personality: "extrovertIntrovert",
 }
 ];
 
--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -1,2 +1,18 @@
 var friendsData = require("../data/friends.js");
-console.log(friendsData);
+
+app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    if (friendsData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
